a0f311d3922e2ea4a29f79501c601fb4e7a02047
hjh <2903791837@qq.com>
2023-07-02 15:59:26 +0800

    Initial commit

---
+++ src/main/java/com/hjh/Main.java
@@ -0,0 +1,12 @@
+package com.hjh;
+
+/**
+ * @author ${USER}
+ * @title ${NAME}
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
