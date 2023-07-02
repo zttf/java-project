d1817f90c0bdcabd1e7ca1cd533ddf90066dc78d
zttf <92673513+zttf@users.noreply.github.com>
2023-07-02 17:21:48 +0800

    Update GitTest.java

--- src/main/java/com/hjh/git/GitTest.java
+++ src/main/java/com/hjh/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master-test");
         System.out.println("hot-fixtest");
+        System.out.println("远程修改");
     }
 }
