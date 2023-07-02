b57f80b28268ac235a21ccd53ab432e1a5a5c816
hjh <2903791837@qq.com>
2023-07-02 16:05:23 +0800

    测试idea提交

---
+++ src/main/java/com/hjh/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.hjh.git;
+
+/**
+ * @author hjh
+ * @title GitTest
+ * @date 2023/7/2 16:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
