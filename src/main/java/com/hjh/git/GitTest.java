e7bcdd4a8a07fd465990c1e24622af730886eff2
hjh <2903791837@qq.com>
2023-07-02 17:20:06 +0800

    测试idea提交

--- src/main/java/com/hjh/git/GitTest.java
+++ src/main/java/com/hjh/git/GitTest.java
@@ -11,9 +11,7 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hot-fix");
-
         System.out.println("master-test");
-
         System.out.println("hot-fixtest");
     }
 }
